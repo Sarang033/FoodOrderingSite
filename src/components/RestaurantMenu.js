7a1d9ee5098934092cc76b6551681fca508dde8a
Sarang Sharma <sarangchamp2--4>
2023-01-21 22:12:31 +0530

    CARD LINKS ENABLED

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../config.js";
+import Shimmer from "./Shimmer.js";
 
 const RestaurantMenu = () => {
     const { id } = useParams();
@@ -9,16 +10,18 @@ const RestaurantMenu = () => {
         getRestaurantInfo();
     }, [])
 
-    const [restaurant, setRestaurant] = useState({});
+    const [restaurant, setRestaurant] = useState(null);
 
     async function getRestaurantInfo() {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/v4/full?lat=28.7338175&lng=77.78487&menuId=662294");
+        const data = await fetch("https://www.swiggy.com/dapi/menu/v4/full?lat=28.7338175&lng=77.78487&menuId="+id);
         const json = await data.json();
         setRestaurant(json.data);
     }
-
-    return (
-        <div>
+    if(!restaurant){
+        return <Shimmer/>
+    }
+    return  (
+        <div className="menu">
             <div>
                 <h1>Restaurant id: {id}</h1>
                 <h2>{restaurant.name}</h2>
@@ -29,7 +32,11 @@ const RestaurantMenu = () => {
                 <h3>{restaurant.costForTwoMsg}</h3>
             </div>
             <div>
-
+                <h1>Menu</h1>
+                <ul>
+                   {Object.values(restaurant?.menu?.items).map((item) => (
+                   <li key={item.id}>{item.name}</li>))}
+                </ul>
             </div>
         </div>
     )
