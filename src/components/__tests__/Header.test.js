cce074b152df3c1ddedab3ae3aeb35c91ab3fdd3
Sarang Sharma <sarangchamp2--4>
2023-02-08 19:39:39 +0530

    added React Testing Lib and wrote some test cases

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+import Header from "../Header"
+
+test("Logo should load on rendering header", ()=>{
+    // Load header
+    const header=render(<Header/>)
+
+
+    //Check if logo is loaded
+    
+})
