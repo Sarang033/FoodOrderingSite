b230b7b5d8ef012ebdc3e0ff27368f3daa19cdd7
SARANG SHARMA <89595882+Sarang033@users.noreply.github.com>
2024-05-10 12:34:06 +0530

    Update data.js

--- src/components/mocks/data.js
+++ src/components/mocks/data.js
@@ -1,1 +1,1 @@
-export const RESTAURANT_DATA ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.73897&lng=77.7789817&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANT_DATA ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.744445219689446&lng=77.77911443263291&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
